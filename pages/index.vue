d15020f2886b96be30983751b8ef8c20d339c2c1
Elijah <elijahbus.ny@gmail.com>
2023-08-10 02:35:53 +0300

    Project templating

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="feeds-scroll">
+
+  </div>
+
+  <div class="landing-content">
+    <div class="main-navigation">
+      <div class="navigation-menu">
+        <a class="menu-item" href="#">HOME</a>
+        <a class="menu-item" href="#">ARTICLES</a>
+        <a class="menu-item" href="mailto:leads@devbus.dev?subject=Hi DevBus.Dev team 👋">CONTACT</a>
+        <a class="join-community-menu-item" href="#">Join The Community</a>
+      </div>
+    </div>
+
+    <div class="landing-intro">
+      <div class="founder-intro__wrapper">
+        <span class="divider-separator"></span>
+        <h3>With <code class="founder-tag">#elijahbus</code></h3>
+      </div>
+
+      <div class="brand-description__wrapper">
+        <h2 class="brand-description">
+          Awareness of the tools you need to thrive in production
+        </h2>
+      </div>
+
+      <div class="resources-link__wrapper">
+        <a href="#">Find all articles</a>
+      </div>
+    </div>
+
+    <div class="social-medias__wrapper">
+      <h4 class="title">Subscribe and follow us on social media</h4>
+
+      <div class="social-medias__cards">
+        <div class="social-media__card">
+          <a class="social-media-link" href="https://youtube.com/@developerbus" target="_blank">
+            <img src="/svg/youtube.svg" alt="Youtube icon">
+            <span style="margin-left: 5px">@developerbus</span>
+          </a>
+        </div>
+
+        <div class="social-media__card">
+          <a class="social-media-link" href="https://twitter.com/elijahbus" target="_blank">
+            <img src="/svg/twitter-svgrepo-com.svg" alt="Twitter icon">
+            <span style="margin-left: 5px">@elijahbus</span>
+          </a>
+        </div>
+
+        <div class="social-media__card">
+          <a class="social-media-link" href="https://github.com/elijahbus" target="_blank">
+            <img src="/svg/github-142-svgrepo-com.svg" alt="Github icon">
+            <span style="margin-left: 5px">/elijahbus</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

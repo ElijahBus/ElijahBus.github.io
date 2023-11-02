cf16d347c04c54520c9a555259bb3821cf369034
Elijah <elijahbus.ny@gmail.com>
2023-11-03 00:52:32 +0300

    Fix cronitor analytics installation

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      src: 'https://rum.cronitor.io/script.js',
+      async: true
+    },
+    {
+      innerHTML: `
+        window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
+        cronitor('config', { clientKey: '554a9c7c1b46f81fddea3842771416b0' });
+      `
+    },
+  ]
+})
+</script>
+
 <template>
   <div class="main">
     <NuxtLayout>

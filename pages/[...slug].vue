e30fabb8d519b27ac3a94e63a603d15e335a7cac
Elijah <elijahbus.ny@gmail.com>
2023-11-03 01:18:59 +0300

    Fix article page and header responsiveness

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -6,7 +6,7 @@ definePageMeta({
 
 <template>
   <main class="flex justify-center p-4 w-100 bg-gray-100 text-sm">
-    <div class="flex flex-col w-[70%] py-20">
+    <div class="flex flex-col w-[90%] md:w-[70%] lg:w-[70%]  py-20">
       <ContentDoc/>
     </div>
   </main>

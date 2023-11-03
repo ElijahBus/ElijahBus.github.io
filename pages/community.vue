af3f58c63c9b4a145a68304a37b5ac8516a6ab8e
Elijah <elijahbus.ny@gmail.com>
2023-11-03 15:15:46 +0300

    Update community page link

--- pages/community.vue
+++ pages/community.vue
@@ -18,8 +18,8 @@ onMounted(() => {
     <p>Redirecting to the server...</p>
     <p>If you are not automatically redirected, click on the link below or copy and paste it in your browser </p>
     <p>
-      <a href="https://discord.gg/JQVHpR7P5X" target="_blank" class="text-blue-900 underline underline-offset-4">
-        https://discord.gg/JQVHpR7P5X
+      <a href="https://www.meetup.com/devseccon-kenya/" target="_blank" class="text-blue-900 underline underline-offset-4">
+        https://www.meetup.com/devseccon-kenya/
       </a>
     </p>
 
